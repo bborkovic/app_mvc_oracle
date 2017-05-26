eabf46ca6f3927e6ec81a446dbe93041363bd48b
Boris Borkovic <bborkovic@gmail.com>
2017-05-26 09:02:35 +0200

    commited

--- public/test.php
+++ public/test.php
@@ -1,9 +1,18 @@
 <?php
+   // function double_string($el) { 
+   //    return $el . $el; 
+   // }
+   
+   $double_string = function($item) {
+       return $item . $item;
+   };
 
-   $dt = '25.10.2016 10:30';
-   echo $dt;
-   $starttime = DateTime::createFromFormat( 'd.m.Y H:i', $dt);
-   var_dump($starttime);
+   echo call_user_func($double_string, "jedan");
+
+   // print_r($arr);
+   // $arr2 = array_map( function($el) { return $el . $el; } , $arr);
+   // echo "<br/>";
+   // print_r( $arr2);
 
  ?>
 
